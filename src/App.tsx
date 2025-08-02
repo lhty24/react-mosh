348f928ed8c0150681987c0ebd3e7491817f7ec9
lhty24 <lianghengty@gmail.com>
2025-08-02 19:18:53 -0400

    dev: implement alert - up to lecture 3.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
+import React from "react";
 
 function App() {
-  let items = ["New York", "San Fran", "Tokyo", "London", "Paris"];
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
     </div>
   );
 }
---
+++ src/components/Alert.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import React from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+const Alert = ({ children }: Props) => {
+  return <div className="alert alert-primary">{children}</div>;
+};
+
+export default Alert;
