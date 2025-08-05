4dc713c0bc46560e3b6acaeababef15a349e79b1
lhty24 <lianghengty@gmail.com>
2025-08-04 22:05:28 -0400

    dev: exercise update state 3 - lec5.12

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
 import { useState } from "react";
 
 function App() {
-  const [pizza, setPizza] = useState({
-    name: "Spicy Pepperoni",
-    toppings: ["Mushroom"],
+  const [cart, setCart] = useState({
+    discount: 0.1,
+    items: [
+      { id: 1, title: "Product 1", quantity: 1 },
+      { id: 2, title: "Product 2", quantity: 1 },
+    ],
   });
 
   const handleClick = () => {
-    setPizza({ ...pizza, toppings: [...pizza.toppings, "Cheese"] });
+    setCart({
+      ...cart,
+      items: cart.items.map((item) =>
+        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
+      ),
+    });
   };
 
   return (
     <div>
-      {pizza.name}
-      <div>{pizza.toppings}</div>
-      <button onClick={handleClick}>Add Toppings</button>
+      Cart
+      <div>Discount: {cart.discount}</div>
+      {cart.items.map((item) => (
+        <li key={item.id}>
+          {item.title} {item.quantity}
+        </li>
+      ))}
+      <button onClick={handleClick}>Update Cart</button>
     </div>
   );
 }
