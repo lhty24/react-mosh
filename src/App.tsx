4f988023623c206fc103b44d503a591742043d9f
lhty24 <lianghengty@gmail.com>
2025-08-04 22:20:39 -0400

    dev: exercise expandable text component - lec5.13

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
 import { useState } from "react";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
   return (
     <div>
-      Cart
-      <div>Discount: {cart.discount}</div>
-      {cart.items.map((item) => (
-        <li key={item.id}>
-          {item.title} {item.quantity}
-        </li>
-      ))}
-      <button onClick={handleClick}>Update Cart</button>
+      <ExpandableText>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus rem
+        obcaecati dolore laboriosam dolor necessitatibus consequatur est
+        perspiciatis maiores. Sequi ullam recusandae placeat necessitatibus
+        cupiditate odit, quisquam neque distinctio praesentium facilis dolores
+        voluptatem perferendis deserunt animi quia libero doloribus asperiores
+        expedita eligendi fugiat est ratione, excepturi voluptates. Accusamus
+        distinctio officia, exercitationem fugit inventore iste dolorem dolor
+        perspiciatis voluptas officiis facilis beatae ad labore tenetur sequi
+        esse iusto. Iste quasi non, veniam corporis nostrum pariatur nam
+        voluptatum. Ab culpa quas sapiente rem libero harum excepturi eveniet,
+        ratione deleniti facere in, quam minus unde est commodi similique odit,
+        adipisci quidem esse iusto.
+      </ExpandableText>
     </div>
   );
 }
