53f41c793ceaebb9e84775fce41c709e25c689e2
lhty24 <lianghengty@gmail.com>
2025-08-04 20:44:45 -0400

    dev: updating arrays - lec5.8

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,21 @@
 import { useState } from "react";
 
 function App() {
-  const [customer, setCustomer] = useState({
-    name: "John",
-    address: {
-      city: "Toronto",
-      zipCode: 111111,
-    },
-  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
 
-  // the spread operator is shallow
-  // make sure the new state object is completely independent of the old state object, they don't share anything
-  // set address to a new object
   const handleClick = () => {
-    setCustomer({
-      ...customer,
-      address: { ...customer.address, zipCode: 666666 },
-    });
+    // Add
+    setTags([...tags, "exciting"]);
+
+    // Remove
+    setTags(tags.filter((tag) => tag !== "happy"));
+
+    // Update
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
   };
 
   return (
     <div>
-      {customer.address.zipCode}
       <button onClick={handleClick}>Update</button>
     </div>
   );
