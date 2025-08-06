878129b325862af12f04d1531f658e8e7e768c67
lhty24 <lianghengty@gmail.com>
2025-08-05 23:37:42 -0400

    dev: 1.apply validation with react hook form 2.use interface to define the shape of the form - lec6.7

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,8 +1,20 @@
 import React, { FormEvent, useRef, useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+interface FormData {
+  name: string;
+  age: number;
+}
+
 const Form = () => {
-  const { register, handleSubmit } = useForm();
+  // nested destructuring
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  console.log(errors);
 
   const onSubmit = (data: FieldValues) => console.log(data);
 
@@ -13,11 +25,20 @@ const Form = () => {
           Name
         </label>
         <input
-          {...register("name")}
+          {...register("name", { required: true, minLength: 3 })}
           id="name"
           type="text"
           className="form-control"
         />
+        {/* ?: optional chaining */}
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name field must be at least 3 characters.
+          </p>
+        )}
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
