d4ef030aab39bcf78e5695fd7a57b84a0997c405
lhty24 <lianghengty@gmail.com>
2025-08-08 00:19:23 -0400

    dev: 1.building expenselist 2.fix previous commit styling issue - lec6.11

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "bootstrap/dist/css/bootstrap-grid.css";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
