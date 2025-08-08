8f389c3fe923d1692938fd0728a504d6f1a924cc
lhty24 <lianghengty@gmail.com>
2025-08-08 17:43:04 -0400

    effect hook - lec7.2

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,28 @@
-import { useState } from "react";
-import ExpenseList from "./expense-tracker/components/ExpenseList";
-import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
-import ExpenseForm from "./expense-tracker/components/ExpenseForm";
-import categories from "./expense-tracker/categories";
+import React, { useEffect, useRef } from "react";
 
-function App() {
-  const [selectedCategory, setSelectedCategory] = useState("");
+const App = () => {
+  const ref = useRef<HTMLInputElement>(null);
 
-  const [expenses, setExpenses] = useState([
-    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
-    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
-    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
-    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
-  ]);
+  // // Side effect: changing state of DOM (something outside of the component)
+  // // Makes the component no longer a pure component
+  // // Use useEffect hook to resolve this
+  // if (ref.current) ref.current.focus();
 
-  // dont use state variables on the things can be calculated from existing state variables
-  const visibleExpenses = selectedCategory
-    ? expenses.filter((e) => e.category === selectedCategory)
-    : expenses;
+  // afterRender
+  useEffect(() => {
+    // Side effect
+    if (ref.current) ref.current.focus();
+  });
+
+  useEffect(() => {
+    document.title = "My App";
+  });
 
   return (
     <div>
-      <div className="mb-5">
-        <ExpenseForm
-          onSubmit={(expense) =>
-            setExpenses([...expenses, { ...expense, id: expenses.length + 1 }])
-          }
-        />
-      </div>
-      <div className="mb-3">
-        <ExpenseFilter
-          onSelectCategory={(category) => setSelectedCategory(category)}
-        />
-      </div>
-      <ExpenseList
-        expenses={visibleExpenses}
-        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
-      />
+      <input ref={ref} type="text" className="form-control" />
     </div>
   );
-}
+};
 
 export default App;
